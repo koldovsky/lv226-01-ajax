577c38e02e4c161bc1d72b779dceed1b386d7e35
koldovsky <koldovsky@yahoo.com>
2017-03-03 15:58:15 +0000

    Form added

---
+++ js/form.js
@@ -0,0 +1,18 @@
+$(function() {
+    $('#btn-submit').click(function(e) {
+       e.preventDefault();
+       $.ajax({
+            url: "https://formspree.io/YOUR-EMAIL-HERE", 
+            method: "POST",
+            data: {
+                clientName: $('#client-name').val(),
+                clientEmail: $('#client-email').val(),
+                comment: $('#comment').val()
+            },
+            dataType: "json"
+       })
+         .done(function() {
+             $('form').html('<h1>Thank you for contacting us!<h1>');
+         });
+    });    
+});
